2a7a936cf785e2c3c57de5665cac1ca7589c4894
aronlopes <aronlopes@gmail.com>
2019-10-31 08:48:43 -0300

    [New] Cannon is cannon.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //private CharacterController controller;
+    public GameObject bulletPrefab;
+    Transform cannonHolder;
+
+    void Start () {
+        //controller = GetComponent<CharacterController> ();
+        cannonHolder = transform.Find ("CannonHolder");
+    }
+
+    void Update () {
+        if (Input.GetKey (KeyCode.E))
+            RotateCannon (-5);
+        else if (Input.GetKey (KeyCode.Q))
+            RotateCannon (5);
+
+    }
+
+    void FixedUpdate () {
+        // Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+        // float speed = 10.0f;
+        // Debug.Log (move * Time.deltaTime * speed);
+        // controller.Move (move * Time.deltaTime * speed);
+    }
+    void RotateCannon (float rotation) {
+        cannonHolder.Rotate (new Vector3 (0, 0, rotation));
+    }
+}
---
+++ Assets/Scripts/ShootBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBehavior : MonoBehaviour {
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown ("space")) {
+            ShootBullet ();
+        }
+    }
+
+    void ShootBullet () {
+        GameObject bullet = Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D> ().AddForce (transform.right * 10.0f, ForceMode2D.Impulse);
+    }
+}
